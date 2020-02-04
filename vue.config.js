c1a104a20d52d061daf751d9d1d6ea99b64c2c68
cobaoyuchao <513072524@qq.com>
2020-02-04 17:21:54 +0800

    update modules and router config

--- vue.config.js
+++ vue.config.js
@@ -124,5 +124,14 @@ module.exports = {
           config.optimization.runtimeChunk('single')
         }
       )
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 }
